5b411adecb5fa6e3cdede5fde5751e250abd24c9
Baris <yurtcu@gmail.com>
2017-02-08 06:29:59 +0300

    Splash screen implemented.

--- app/src/main/java/com/yurtcu/operareddittask/MainActivity.java
+++ app/src/main/java/com/yurtcu/operareddittask/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
---
+++ app/src/main/java/com/yurtcu/operareddittask/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.yurtcu.operareddittask;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);    }
+}
